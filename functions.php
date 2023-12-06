0ca3f28c1d3e2eaa352459474e1b5395ac56c6b7
benbegins <62623777+benbegins@users.noreply.github.com>
2023-12-06 13:54:08 +0100

    custom login page

---
+++ functions.php
@@ -0,0 +1,31 @@
+<?php
+function custom_login() { 
+?>
+    <style type="text/css">
+        body.login{
+            background: #EDEAE4;
+            display: flex;
+            align-items: center;
+        }
+
+        body.login div#login{
+            padding: 0;
+        }
+
+        body.login div#login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/src/images/logo-bemy.png);
+            height:48px;
+            width:140px;
+            background-size: 140px 48px;
+            background-repeat: no-repeat;
+        }
+
+        body.login div#login form#loginform p.submit input#wp-submit {
+            background: #F54F4F;
+            border: 0;
+        }
+    </style>
+<?php 
+}
+
+add_action( 'login_enqueue_scripts', 'custom_login' );
